549f5e22d9904d3560473174dfece11b7bcdaef1
rishikksh20 <rishikksh20@gmail.com>
2016-01-17 00:15:18 +0530

    Add CandleStick OHLC graphs

--- matplotlibPlots.py
+++ matplotlibPlots.py
@@ -98,3 +98,68 @@ plt.pie(slices,
 plt.title('Intresting Graph \n Check it out')
 #plt.legend()
 plt.show()
+
+
+# CandleStick OHLC graphs
+
+import numpy as np
+import urllib2
+import matplotlib.dates as mdates
+import matplotlib.ticker as mticker
+from matplotlib.finance import candlestick_ohlc
+
+def stockData(stock):
+    fig=plt.figure()
+    subPlot=plt.subplot2grid((1,1),(0,0))
+    stock_price_url='http://chartapi.finance.yahoo.com/instrument/1.0/'+stock+'/chartdata;type=quote;range=1m/csv'    
+    source_req=urllib2.Request(stock_price_url)
+    source_response=urllib2.urlopen(source_req)
+    source_code=source_response.read().decode()
+    
+    stock_data=[]
+    split_source=source_code.split('\n')
+    
+    for  line in split_source:
+        split_line=line.split(',')
+        if len(split_line)==6:
+            if 'values' not in line and 'labels' not in line:
+                stock_data.append(line)
+                
+                
+    date,closep,highp,lowp,openp,volume=np.loadtxt(stock_data,
+                                              delimiter=',',
+                                              unpack=True,
+                                              converters={0: bytespdate2num('%Y%m%d')})
+        
+    x=0
+    y=len(date)
+    ohlc=[]
+    
+    while x<y:
+        append_me=date[x],openp[x],highp[x],lowp[x],closep[x],volume[x]
+        ohlc.append(append_me)
+        x+=1
+        
+    candlestick_ohlc(subPlot,ohlc,width=0.4,colorup='#77d899',colordown='#db4f3f')
+    
+    for label in subPlot.xaxis.get_ticklabels():
+        label.set_rotation(45)
+        
+    subPlot.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    subPlot.xaxis.set_major_locater(mticker.MaxNLocator(10))
+    
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Intresting Graph \n Check it out')
+    plt.show()
+    
+def bytespdate2num(fmt, encoding='utf-8'):
+    strconverter = mdates.strpdate2num(fmt)
+    def bytesconverter(b):
+        s = b.decode(encoding)
+        return strconverter(s)
+    return bytesconverter
+    
+
+
+stockData('TSLA') #forecasting tesla stock prices
